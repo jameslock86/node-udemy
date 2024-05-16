23a7f8edcaca99538367581d09e5e9cc78575d95
james lockwood <ksjjl05@gmail.com>
2024-05-16 14:38:29 -0500

    node server work

--- lecture/dataResponse.js
+++ lecture/dataResponse.js
@@ -1,11 +1,35 @@
 const { log } = require('console');
 const http = require('http');
 
+const data = [
+    {
+        id:1,
+        name:'john'
+    },
+    {
+        id:2,
+        name:'tim'
+    },
+    {
+        id:3,
+        name:'pam'
+    },
+    {
+        id:4,
+        name:'joe'
+    }
+]
 const server = http.createServer((req, res) => {
     // log('first server');
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Content-Language', 'en-us');
+    res.setHeader('Date',new Date(Date.now()));
+    res.setHeader('X-Powered-By', 'Node.js');
     res.end(
        JSON.stringify( {
-        message: 'data us sent'
+        success:true,
+        results :data.length,
+        data: data
     }))
 
 });
